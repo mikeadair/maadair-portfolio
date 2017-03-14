3088cdd38e53f71d145398284769513493486c19
mikeadair <michaeladair1996@gmail.com>
2017-03-14 04:13:36 -0400

    minor edits

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,7 +17,8 @@ app.config(function($stateProvider, $urlRouterProvider) {
 
         .state('contact', {
             url: '/contact',
-            templateUrl: 'assets/html/contact.html'
+            templateUrl: 'assets/html/contact.html',
+            controller: 'ContactController'
         });
         
     
@@ -25,4 +26,38 @@ app.config(function($stateProvider, $urlRouterProvider) {
 
 app.controller('PageController', function($scope) {
     
+});
+
+app.controller('ContactController', function($scope, $http) {
+    $scope.subject;
+    $scope.email;
+    $scope.message;
+    $scope.error;
+    $scope.issue;
+    
+    $scope.submit = function(){
+        if($scope.email != null && $scope.email != "" && $scope.email.match(/^[_a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]*\.([a-z]{2,4})$/)){
+            if($scope.subject != null && $scope.subject != ""){
+                if($scope.message != null && $scope.message != ""){
+                    $scope.error = null;
+                    $scope.issue = null;
+                    var data = $.param({
+                        message: $scope.message,
+                        subject: $scope.subject,
+                        email: $scope.email
+                    });
+                    $http.post('http://api.maadair.com:8888/sendmessage',data);
+                }else{
+                    $scope.error = "Message field is empty!";
+                    $scope.issue = "message";
+                }
+            }else{
+                $scope.error = "Subject field is empty!";
+                $scope.issue = "subject";
+            }
+        }else{
+            $scope.error = "Please give a valid E-mail Address!";
+            $scope.issue = "email";
+        }
+    }
 });
